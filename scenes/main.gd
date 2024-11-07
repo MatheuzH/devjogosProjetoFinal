e65778e98ff05fe6715fccb178489aca71e18896
Matheus Herzog <matheusgadelha01@gmail.com>
2024-11-07 13:23:41 -0300

    capivara

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-#preload obstacles
+
 var stump_scene = preload("res://scenes/stump.tscn")
 var rock_scene = preload("res://scenes/rock.tscn")
 var barrel_scene = preload("res://scenes/barrel.tscn")
@@ -9,8 +9,8 @@ var obstacle_types := [stump_scene, rock_scene, barrel_scene]
 var obstacles : Array
 var bird_heights := [200, 390]
 
-#game variables
-const DINO_START_POS := Vector2i(150, 485)
+
+const CAPIVARA_START_POS := Vector2i(150, 485)
 const CAM_START_POS := Vector2i(576, 324)
 var difficulty
 const MAX_DIFFICULTY : int = 2
@@ -26,7 +26,7 @@ var ground_height : int
 var game_running : bool
 var last_obs
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	screen_size = get_window().size
 	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
@@ -34,53 +34,53 @@ func _ready():
 	new_game()
 
 func new_game():
-	#reset variables
+	#reseta variaveis
 	score = 0
 	show_score()
 	game_running = false
 	get_tree().paused = false
 	difficulty = 0
 	
-	#delete all obstacles
+	#deleta todos os obstaculos
 	for obs in obstacles:
 		obs.queue_free()
 	obstacles.clear()
 	
-	#reset the nodes
-	$Dino.position = DINO_START_POS
-	$Dino.velocity = Vector2i(0, 0)
+	#reseta os nodes
+	$capivara.position = CAPIVARA_START_POS
+	$capivara.velocity = Vector2i(0, 0)
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0, 0)
 	
-	#reset hud and game over screen
+	#reseta o hud e tela de game over
 	$HUD.get_node("StartLabel").show()
 	$GameOver.hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	if game_running:
-		#speed up and adjust difficulty
+		#aumenta velocidade e ajusta dificuldade
 		speed = START_SPEED + score / SPEED_MODIFIER
 		if speed > MAX_SPEED:
 			speed = MAX_SPEED
 		adjust_difficulty()
 		
-		#generate obstacles
+		#gera obstaculos
 		generate_obs()
 		
-		#move dino and camera
-		$Dino.position.x += speed
+		#move a capivara e a camera
+		$capivara.position.x += speed
 		$Camera2D.position.x += speed
 		
-		#update score
+		#atualizar score
 		score += speed
 		show_score()
 		
-		#update ground position
+		#atualizar a posição do chão
 		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
 			$Ground.position.x += screen_size.x
 			
-		#remove obstacles that have gone off screen
+		#remove obstaculos quando sair da tela
 		for obs in obstacles:
 			if obs.position.x < ($Camera2D.position.x - screen_size.x):
 				remove_obs(obs)
@@ -90,7 +90,7 @@ func _process(delta):
 			$HUD.get_node("StartLabel").hide()
 
 func generate_obs():
-	#generate ground obstacles
+	#gera abstaculos terrestres 
 	if obstacles.is_empty() or last_obs.position.x < score + randi_range(300, 500):
 		var obs_type = obstacle_types[randi() % obstacle_types.size()]
 		var obs
@@ -103,10 +103,10 @@ func generate_obs():
 			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 5
 			last_obs = obs
 			add_obs(obs, obs_x, obs_y)
-		#additionally random chance to spawn a bird
+		#adiciona chace aleátoria de spawnar pássaro
 		if difficulty == MAX_DIFFICULTY:
 			if (randi() % 2) == 0:
-				#generate bird obstacles
+				#gera abstaculo de pássaro
 				obs = bird_scene.instantiate()
 				var obs_x : int = screen_size.x + score + 100
 				var obs_y : int = bird_heights[randi() % bird_heights.size()]
@@ -123,7 +123,7 @@ func remove_obs(obs):
 	obstacles.erase(obs)
 	
 func hit_obs(body):
-	if body.name == "Dino":
+	if body.name == "capivara":
 		game_over()
 
 func show_score():
